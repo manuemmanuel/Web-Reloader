2e1a88e7eb62061f1e25844dc6e3784c98b85b09
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2024-01-05 23:16:40 +0530

    Update webreloader.py

--- webreloader.py
+++ webreloader.py
@@ -1,16 +1,17 @@
 import requests
 import time
 
-def reload_url(url, count):
+def reload_url(url, count, delay):
     try:
         for i in range(count):
             response = requests.get(url)
             print(f"Reload count: {i + 1}")
-            time.sleep(1)
+            time.sleep(delay)
     except requests.RequestException as e:
         print(f"Error: {e}")
 
 if __name__ == "__main__":
     url = input("Enter the URL: ")
     count = int(input("How many times do you want to reload? "))
-    reload_url(url, count)
+    delay = int(input("Enter the delay(s): "))
+    reload_url(url, count, delay)
